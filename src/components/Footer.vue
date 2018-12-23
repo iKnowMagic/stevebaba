de2223f08690dd4e3d1bad2693b2a9b6d1660fe5
iknowmagic <ilan@iknowmagic.net>
2018-12-22 21:05:35 -0800

    theme mostly done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,36 +7,7 @@
     </div>
     <div class="container">
       <div class="row mb50">
-        <div class="col-md-5">
-          <div class="probootstrap-footer-widget">
-            <h4>About The Site</h4>
-            <p>
-              The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks
-              and devious Semikoli. <a href="#">Learn More</a>
-            </p>
-          </div>
-        </div>
-        <div class="col-md-7">
-          <div class="probootstrap-footer-widget">
-            <h4>Links</h4>
-            <ul class="probootstrap-footer-link float">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Books</a></li>
-            </ul>
-            <ul class="probootstrap-footer-link float">
-              <li><a href="#">About Me</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-          <div class="probootstrap-footer-widget">
-            <h4>Contact Info</h4>
-            <ul class="probootstrap-contact-info">
-              <li><i class="icon-location2"></i> <span>198 West 21th Street, Suite 721 New York NY 10016</span></li>
-              <li><i class="icon-mail"></i><span>info@domain.com</span></li>
-              <li><i class="icon-phone2"></i><span>+123 456 7890</span></li>
-            </ul>
-          </div>
-        </div>
+        <div class="col"><div class="probootstrap-footer-widget"></div></div>
       </div>
     </div>
   </footer>
